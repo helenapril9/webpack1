6ce20d3b0f9626386e23110cb8a260d6edb850d2
Nelly Porvatova <dolgikhhel@yandex.ru>
2023-09-14 21:34:47 +0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    module: {
+        rules:[
+            {
+                test: /\.css$/,
+                use:[
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]            
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCSSExtractPlugin()
+    ]
+}
